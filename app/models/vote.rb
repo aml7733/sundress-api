645f99323b3d176b50ca929d898c98b4bbded055
Adam <lucia.adam@gmail.com>
2018-07-20 21:47:05 -0400

    add vote and runway_model controllers

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :vote_runway_models
   has_many :runway_models, through: :vote_runway_models
 end
